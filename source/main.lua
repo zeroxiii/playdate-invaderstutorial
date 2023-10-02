cb3b54f82abbf2031767284997f4ee910306e585
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2023-10-02 00:45:04 -0400

    Created initial main.lua file

--- source/main.lua
+++ source/main.lua
@@ -4,3 +4,11 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+function pd.update()
+    gfx.sprite.update()
+    pd.timer.updateTimers()
+end
+
